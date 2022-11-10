b22d0085c8099d88c5e67dda29edf6c6abf57825
Rahul <rahulpatidar0191@gmail.com>
2022-11-10 15:17:02 -0800

    remove deploy for now

--- script/DeployPRorTag.sh
+++ script/DeployPRorTag.sh
@@ -34,15 +34,16 @@ deploy_pr_branch_or_tag() {
 
     #TODO : PR theme links  
     cd src
+    ls
     echo "Running deploy command"
-    docker run satel/themekit:1.2-alpha1 theme deploy --password=${THEMEKIT_PASSWORD} --store=${STORE_NAME} --themeid=${THEME_ID}  --env ${THEME_ENV}; STATUS1=$?     
+    #docker run satel/themekit:1.2-alpha1 theme deploy --password=${THEMEKIT_PASSWORD} --store=${STORE_NAME} --themeid=${THEME_ID}  --env ${THEME_ENV}; STATUS1=$?     
 
     #To overcome first theme deploy's limitation for V2 of uploading files in a bad order, so deploy once again
-    if [[ $STATUS1 != 0 ]]
-    then    
-        echo "THEME DEPLOY LOOP"
-        docker run satel/themekit:1.2-alpha1 theme deploy --env ${THEME_ENV}
-    fi    
+    # if [[ $STATUS1 != 0 ]]
+    # then    
+    #     echo "THEME DEPLOY LOOP"
+    #     docker run satel/themekit:1.2-alpha1 theme deploy --env ${THEME_ENV}
+    # fi    
 }   
 
 function configure_theme(){
